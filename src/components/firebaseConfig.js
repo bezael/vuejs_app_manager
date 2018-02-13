8b944cfaac617bd6bca1655274b765cd1d197731
Bezael Pérez <bezael_e@hotmail.com>
2018-02-13 19:11:47 +0100

    Vuejs app

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+	    apiKey: "AIzaSyB7J0jYOUWGYV5RzpdCKrFyKnbm8B4Rk-c",
+	    authDomain: "vuedev-540e3.firebaseapp.com",
+	    databaseURL: "https://vuedev-540e3.firebaseio.com",
+	    projectId: "vuedev-540e3",
+	    storageBucket: "vuedev-540e3.appspot.com",
+	    messagingSenderId: "818285973901"
+}
